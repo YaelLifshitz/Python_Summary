9ac5ce426f0609254dd45cf65050626dc335702e
YaelLifshitz <64844458+YaelLifshitz@users.noreply.github.com>
2020-10-06 15:39:29 +0300

    Initial commit

---
+++ Numpy.py
@@ -0,0 +1,6 @@
+'''
+This is a summary of Numpy.
+I summarized the following video: https://www.youtube.com/watch?v=WGJJIrtnfpk&list=PL9ooVrP1hQOHY-BeYrKHDrHKphsJOyRyu
+I did this for my myselfe so I can always go back and remember what I have learned.
+I encourage everyone to go watch the video, like, subscribe, and show them love in the comment section!
+'''
